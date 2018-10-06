deb4a48b90676679178c10d509589220ae53a90d
Brandon Wade <brandon_wade2005@hotmail.com>
2018-10-06 14:04:31 -0500

    Add react router and home page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,14 @@
 import React, { Component } from 'react';
-import Calendar from './components/Calendar';
-import Details from './components/Details';
-import moment from 'moment';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            selectedDate: moment().format('MMMM Do YYYY'),
-        };
-    }
-
-    setSelectedDate = (date) => {
-        this.setState({
-            selectedDate: moment(date).format('MMMM Do YYYY'),
-        });
-    };
-
     render() {
         return (
-            <div className='App'>
-                <div className='App-content'>
-                    <Calendar
-                        setSelectedDate={this.setSelectedDate}
-                    />
-                    <Details
-                        selectedDate={this.state.selectedDate}
-                    />
-                </div>
-            </div>
+            <BrowserRouter>
+                <Route path='/' component={Home} />
+            </BrowserRouter>
         );
     }
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Calendar from '../components/Calendar';
+import Details from '../components/Details';
+import moment from 'moment';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedDate: moment().format('MMMM Do YYYY'),
+        };
+    }
+
+    setSelectedDate = (date) => {
+        this.setState({
+            selectedDate: moment(date).format('MMMM Do YYYY'),
+        });
+    };
+
+    render() {
+        return (
+            <div className='App'>
+                <div className='App-content'>
+                    <Calendar
+                        setSelectedDate={this.setSelectedDate}
+                    />
+                    <Details
+                        selectedDate={this.state.selectedDate}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
