baf7030d49c04c9406ab1212045f6853bc4ee12b
Brandon Wade <brandon_wade2005@hotmail.com>
2018-12-27 19:26:21 -0600

    Add logging and fix golint comment warnings in expense controller.

--- api/controllers/expenses.go
+++ api/controllers/expenses.go
@@ -8,8 +8,11 @@ import (
 	"github.com/BrandonWade/enako/api/models"
 	"github.com/BrandonWade/enako/api/services"
 	"github.com/gorilla/mux"
+
+	log "github.com/sirupsen/logrus"
 )
 
+// ExpenseController the interface for expense related APIs
 //go:generate counterfeiter -o fakes/fake_expense_controller.go . ExpenseController
 type ExpenseController interface {
 	GetExpenses(w http.ResponseWriter, r *http.Request)
@@ -22,6 +25,7 @@ type expenseController struct {
 	service services.ExpenseService
 }
 
+// NewExpenseController the constructor for a new ExpenseController
 func NewExpenseController(service services.ExpenseService) ExpenseController {
 	return &expenseController{
 		service,
@@ -33,7 +37,13 @@ func (e *expenseController) GetExpenses(w http.ResponseWriter, r *http.Request) 
 
 	expenses, err := e.service.GetExpenses(userAccountID)
 	if err != nil {
-		// TODO: Handle
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.GetExpenses",
+			"account ID": userAccountID,
+			"err":        err.Error(),
+		}).Error("error fetching expense list")
+
+		// TODO: Return an error response
 	}
 
 	json.NewEncoder(w).Encode(expenses)
@@ -45,12 +55,24 @@ func (e *expenseController) CreateExpense(w http.ResponseWriter, r *http.Request
 	var expense models.UserExpense
 	err := json.NewDecoder(r.Body).Decode(&expense)
 	if err != nil {
-		// TODO: Handle
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.CreateExpense",
+			"account ID": userAccountID,
+			"err":        err.Error(),
+		}).Error("error decoding expense payload")
+
+		// TODO: Return an error response
 	}
 
 	ID, err := e.service.CreateExpense(userAccountID, &expense)
 	if err != nil {
-		// TODO: Handle
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.CreateExpense",
+			"account ID": userAccountID,
+			"err":        err.Error(),
+		}).Error("error creating expense")
+
+		// TODO: Return an error response
 	}
 
 	expense.ID = ID
@@ -65,22 +87,52 @@ func (e *expenseController) UpdateExpense(w http.ResponseWriter, r *http.Request
 
 	ID, err := strconv.ParseInt(params["id"], 10, 64)
 	if err != nil {
-		// TODO: Handle
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.UpdateExpense",
+			"account ID": userAccountID,
+			"id":         params["id"],
+			"err":        err.Error(),
+		}).Error("error parsing expense id")
+
+		// TODO: Return an error response
 	}
 
 	expense := models.UserExpense{}
 	err = json.NewDecoder(r.Body).Decode(&expense)
 	if err != nil {
-		// TODO: Handle
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.UpdateExpense",
+			"account ID": userAccountID,
+			"id":         ID,
+			"err":        err.Error(),
+		}).Error("error decoding expense payload")
+
+		// TODO: Return an error response
 	}
 
 	count, err := e.service.UpdateExpense(ID, userAccountID, &expense)
 	if err != nil {
-		// TODO: Handle
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.UpdateExpense",
+			"account ID": userAccountID,
+			"id":         ID,
+			"err":        err.Error(),
+		}).Error("error updating expense")
+
+		// TODO: Return an error response
 	}
 
 	if count == 0 {
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.UpdateExpense",
+			"account ID": userAccountID,
+			"id":         ID,
+			"err":        err.Error(),
+		}).Warn("update expense modified 0 rows")
+
+		// TODO: Return an error response
 		json.NewEncoder(w).Encode(&models.UserExpense{})
+
 		return
 	}
 
@@ -96,16 +148,39 @@ func (e *expenseController) DeleteExpense(w http.ResponseWriter, r *http.Request
 
 	ID, err := strconv.ParseInt(params["id"], 10, 64)
 	if err != nil {
-		// TODO: Handle
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.DeleteExpense",
+			"account ID": userAccountID,
+			"id":         ID,
+			"err":        err.Error(),
+		}).Error("error parsing expense id")
+
+		// TODO: Return an error response
 	}
 
 	count, err := e.service.DeleteExpense(ID, userAccountID)
 	if err != nil {
-		// TODO: Handle
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.DeleteExpense",
+			"account ID": userAccountID,
+			"id":         ID,
+			"err":        err.Error(),
+		}).Error("error deleting expense")
+
+		// TODO: Return an error response
 	}
 
 	if count == 0 {
+		log.WithFields(log.Fields{
+			"method":     "ExpenseController.DeleteExpense",
+			"account ID": userAccountID,
+			"id":         ID,
+			"err":        err.Error(),
+		}).Warn("delete expense removed 0 rows")
+
+		// TODO: Return an error response
 		json.NewEncoder(w).Encode(&models.UserExpense{})
+
 		return
 	}
 
